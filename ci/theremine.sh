0a36d13f768dff991dbe0aa76d04b4dfafe3de5b
antoine <antoine.lesqueren.perso@gmail.com>
2017-02-18 12:38:17 +0100

    ci: image building

--- ci/theremine.sh
+++ ci/theremine.sh
@@ -1,3 +1,4 @@
-docker-compose build theremine
-docker tag drive_theremine registry.consomalin.ovh:443/theremine
+docker-compose build --pull theremine
+img=$(pwd | rev | cut -d"/" -f1 | rev | tr '[:upper:]' '[:lower:]')_theremine
+docker tag $img registry.consomalin.ovh:443/theremine
 docker push registry.consomalin.ovh:443/theremine
