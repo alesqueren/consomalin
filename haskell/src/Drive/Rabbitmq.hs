c8bad240d2250262e0987eca87ae32e1efb357f9
antoine <antoine@anywhere>
2017-03-02 11:24:09 +0100

    wendy: rabbitmq triggers findTransaction

--- haskell/src/Drive/Rabbitmq.hs
+++ haskell/src/Drive/Rabbitmq.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Drive.Rabbitmq (RabbitmqResource(..), listen) where
 
 import           Protolude hiding (msg)
@@ -6,15 +8,34 @@ import qualified Data.ByteString.Lazy.Char8 as BL
 import           Drive.Utils
 import qualified Data.Attoparsec.Text as A
 import qualified Data.Text as T
+import           Data.Aeson
+import           GHC.Generics (Generic)
+import           Drive.User
+
+newtype TransactionMessage = TransactionMessage { user :: Text }
+  deriving (Typeable, Show, Eq, Generic)
+
+instance FromJSON TransactionMessage
+instance ToJSON TransactionMessage
+
+processTransactionMessage :: TransactionMessage -> IO ()
+processTransactionMessage tm = do
+  t <- findTransaction $ user tm
+  putStrLn (show t :: Text)
+
 
 
 data RabbitmqResource = TransactionResource
 
 class Queryable a where
   getQueue :: a -> Text
+  -- TODO: no TransactionMessage (rigid type)
+  getCallback :: a -> (TransactionMessage -> IO ())
 
 instance Queryable RabbitmqResource where
   getQueue TransactionResource = "transactions"
+  getCallback TransactionResource = processTransactionMessage
+
 
 listen ::(Queryable r) => r -> IO ()
 listen r = do
@@ -27,21 +48,19 @@ listen r = do
 
   declareQueue chan newQueue {queueName = qname}
 
-  consumeMsgs chan qname Ack myCallback
+  consumeMsgs chan qname Ack $ processMsg $ getCallback r
+  putStrLn ("listening to " <> qname <> "...":: Text)
 
-  publishMsg chan "" qname
-      newMsg {msgBody = BL.pack "hello world",
-              msgDeliveryMode = Just Persistent}
-
-  getLine -- wait for keypress
-  closeConnection conn
-  putStrLn ("connection closed" :: Text)
+  -- closeConnection conn
+  -- putStrLn ("connection closed" :: Text)
 
   where
     qname = getQueue r
 
-
-myCallback :: (Message,Envelope) -> IO ()
-myCallback (msg, env) = do
+processMsg :: (FromJSON a) => (a -> IO ()) -> (Message,Envelope) -> IO ()
+processMsg cb (msg, env) = do
   putStrLn $ "received message: " ++ BL.unpack (msgBody msg)
+  case decode (msgBody msg) of
+    Just tm -> cb tm
+    Nothing -> putStrLn ("parsing error" :: Text)
   ackEnv env
--- haskell/wendy/Main.hs
+++ haskell/wendy/Main.hs
@@ -1,12 +1,9 @@
 module Main where
 
 import           Protolude
-import           Drive.User
 import           Drive.Rabbitmq
 
 main :: IO ()
 main = do
-  t <- findTransaction "az@hotmail.fr"
-  putStrLn (show t :: Text)
-
   listen TransactionResource
+  forever $ threadDelay 10000000
