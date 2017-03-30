dd05c9c40421a490d34f73265771e1ae3b1b84ed
antoine <antoine@anywhere>
2017-03-30 10:35:43 +0200

    theremine: named routes

--- theremine/src/router/index.js
+++ theremine/src/router/index.js
@@ -3,7 +3,6 @@ import Router from 'vue-router';
 import Home from '@/components/Home';
 import Login from '@/components/Login';
 import Register from '@/components/Register';
-import Profile from '@/components/Profile';
 import Wishlist from '@/components/Wishlist';
 import Section from '@/components/Section';
 
@@ -11,36 +10,50 @@ Vue.use(Router);
 Vue.router = new Router({});
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
+      name: 'home',
       path: '/',
       component: Home,
     },
     {
+      name: 'login',
       path: '/login',
       component: Login,
     },
     {
+      name: 'register',
       path: '/register',
       component: Register,
     },
     {
+      name: 'wishlist',
       path: '/wishlist',
+      // meta: { auth: true },
       component: Wishlist,
     },
     {
+      name: 'section',
       path: '/section',
+      // meta: { auth: true },
       component: Section,
     },
     {
-      path: '/profile',
-      meta: { auth: true },
-      component: Profile,
+      name: 'basket',
+      path: '/basket',
+      // meta: { auth: true },
+      // component: Profile,
+    },
+    {
+      name: 'withdraw',
+      path: '/withdraw',
+      // meta: { auth: true },
+      // component: Profile,
     },
     {
       path: '*',
       redirect: '/',
     },
-
   ],
 });
