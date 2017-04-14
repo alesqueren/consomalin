1cfdf78d11f72b6a83bc3c2c04df3b9336cb9f60
francois <fullauto00@gmail.com>
2017-04-14 16:55:15 +0200

    theremine: remove additionnal space

--- theremine/src/components/Home.vue
+++ theremine/src/components/Home.vue
@@ -1,18 +1,25 @@
 <template lang="pug">
   div#homepage
     div#rassurance
-      div.item + Rapide
-      div.item + Moins de produits oubliés
-      div.item + Aucune pub !
+      div.item
+        i.fa.fa-plus-circle
+        span &nbsp;Rapide
+      div.item
+        i.fa.fa-plus-circle
+        span &nbsp;Moins de produits oubliés
+      div.item
+        i.fa.fa-plus-circle
+        span &nbsp;Aucune pub !
     div#presentation
       div.item
-        div.img screenshot liste de course
+        div.imgscreenshot liste de course
         div.txt Fais ta liste de course comme à la maison
       div.item
         div.img screenshot de panier à moitié rempli
         div.txt Choisit tes produits en fonction de ta liste
       div.item
-        div.img image de retrait de drive
+        div.img
+          img.logo(src="../assets/images/withdraw.png")
         div.txt Régle tes achats chez Auchan lors du retrait
 </template>
 
@@ -22,24 +29,32 @@
 <style>
 #rassurance{
   clear: both;
+  padding-top: 25px;
+  text-align: center;
+  font-size: 2.5em;
 }
 #rassurance .item{
+  width: 33%;
   float: left;
 }
+
 #presentation{
+  padding-top: 50px;
   clear: both;
+  text-align: center;
+  font-size: 2em;
 }
 #presentation .item{
-  clear: both;
+  display: table;
 }
 #presentation .item>div{
+  display: table-cell;
   width: 50%;
 }
 #presentation .item .img{
-  float: left;
 }
 #presentation .item .txt{
-  float: right;
   text-align: left;
+  vertical-align: center;
 }
 </style>
